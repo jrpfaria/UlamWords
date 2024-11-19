f883fb8142407d570fc501b1cde98c0df650bc97
Rodrigo Faria <jrfaria@ua.pt>
2024-11-19 22:17:31 +0000

    bitmaps done

--- Bitmaps/ulam.c
+++ Bitmaps/ulam.c
@@ -5,8 +5,8 @@
 
 #define bit(p,x) (p[x / 8] >> (x % 8) & 1) /*Get the xth bit of p*/
 #define set(p,x) p[x / 8] |= 1 << (x % 8) /*Set the xth bit of p to 1*/
-#define ones(x) ((1ULL << i+1) - 1) /*Get a variable of length x with just '1's*/
-#define max_length 23
+#define ones(x) ((1ULL << x) - 1) /*Get a variable of length x with just '1's*/
+#define max_length 30
 
 int is_ulam(uint64_t word, char** UlamWords, int l) {
     int num_reps = 0;
@@ -27,18 +27,32 @@ int main(void)
 {
     // Initialize Variables
     char** UlamWords = (char**)malloc(max_length * sizeof(char*));
-    FILE* fp = fopen("w", "w");
+    FILE* fp = fopen("results.txt", "w");
     uint64_t size = 0;
 
     // Generate Ulam Words
     for (int i = 0, j = 3; i < max_length; i++, size=0)
     {
-        UlamWords[i] = (char*)calloc(sizeof(char)*(ones(i)/8+1),1);
-        if (i < 2) *UlamWords[i] = j<<i, size=2;
+        UlamWords[i] = (char*)calloc(ones(i+1) / 8 + 1, 1);
+        if (UlamWords[i] == NULL) {
+            fprintf(stderr, "Memory allocation failed for UlamWords[%d]\n", i);
+            exit(EXIT_FAILURE);
+        }
+        if (i < 2)
+        { 
+            *UlamWords[i] = j<<i;
+            size=2;
+        }
         else
-            for (uint64_t word = 0; word <= ones(i); word++)
-                if (is_ulam(word, UlamWords, i+1))
-                    set(UlamWords[i], word), size++;
+        {
+            for (uint64_t word = 0; word <= ones(i+1); word++)
+            {   if (is_ulam(word, UlamWords, i+1))
+                {    
+                    set(UlamWords[i], word);
+                    size++;
+                }
+            }
+        }
         fprintf(fp, "Length %d\n%ld\n", i+1, size);
     }
     
