8c3d41ddd08f9fd8e3d3862f664ccf54a70b1966
Rodrigo Faria <jrfaria@ua.pt>
2024-11-20 13:28:24 +0000

    528 characters

--- golf/ulam.c
+++ golf/ulam.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define b(p,x)p[x/8]>>(x%8)&1
-#define s(p,x)p[x/8]|=1<<(x%8)
 #define o(x)((1UL<<(x))-1)
 #define L unsigned long
-int main(void){char**p=(char**)malloc(30*8);FILE*fp=fopen("w","w");L S=0;for(L i=0,j=3;i<30;i++,S=0){p[i]=(char*)malloc(o(i+1)/8+1);if(i<2)*p[i]=j<<i,S=2;else for(L w=0,t=i+1,r=0,h,l;w<=o(i+1);w++,r=0){for(L k=1;k<t;k++){h=w>>(t-k);l=w&o(t-k);if(h!=l||k!=t-k)if(b(p[k-1],h)&&b(p[t-k-1],l))if(++r>1)break;}if(r==1)s(p[i],w),S++;}fprintf(fp,"Length %ld\n%ld\n",i+1,S);}for(L i=0;i<30;i++)free(p[i]);free(p);fclose(fp);return 0;}
+int main(void){char*p[30];FILE*fp=fopen("w","w");L S=0;for(L i=0,j=3;i<30;i++,S=0){p[i]=(char*)malloc(o(i+1)/8+1);if(i<2)*p[i]=j<<i,S=2;else for(L w=0,t=i+1,r=0,h,l;w<=o(i+1);w++,r=0){for(L k=1;k<t;k++){h=w>>(t-k);l=w&o(t-k);if(h!=l||k!=t-k)if(b(p[k-1],h)&&b(p[t-k-1],l))if(r++)break;}if(r==1)p[i][w/8]|=1<<(w%8),S++;}fprintf(fp,"Length %ld\n%ld\n",i+1,S);}for(L i=0;i<30;i++)free(p[i]);fclose(fp);return 0;}
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
-int main(void)
-{
-    printf("%ld\n", sizeof(unsigned long));
-    return 0;
-}
